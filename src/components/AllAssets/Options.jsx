95c939283d44978b71f7c047f4c4662e63f82923
Sairam Tagline <sairam.tagline@gmail.com>
2022-02-14 18:43:41 +0530

    form changes

--- src/components/AllAssets/Options.jsx
+++ src/components/AllAssets/Options.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Description from '../../shared/Description'
+import Form from '../../shared/Form'
+import Title from '../../shared/Title'
 
 const Options = () => {
   return (
-    <div className='pb-4'>Options</div>
+    <div className='pb-4'>
+
+      <Title>
+        Fractional options are the future
+      </Title>
+      <Description>
+        In the future, Structure will allow its users to buy and sell fractions of options.
+      </Description>
+      <Form select={false} btnValue="Subscribe" placeholder="Enter email to stay up to date" />
+    </div>
   )
 }
 
--- src/shared/Form.jsx
+++ src/shared/Form.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { Form as Frm } from 'react-bootstrap'
 import Button from './Button'
 
-const Form = () => {
+const Form = ({ select = true, btnValue = "Send link", placeholder = "Enter your phone number" }) => {
   return (
     <Frm>
-      <Frm.Group className="trading-form">
+      <Frm.Group className="trading-form">{
+        select &&
         <Frm.Select aria-label="Default select example">
           <option>US +1</option>
           <option value="1">AU +43</option>
           <option value="1">RU +7</option>
           <option value="2">SW +46</option>
         </Frm.Select>
-        <Frm.Control type="text" placeholder="Enter your phone number" />
-        <Button variant="primary" type="submit">
-          Send link
+      }
+        <Frm.Control type="text" placeholder={placeholder} />
+        <Button variant="primary" type="button">
+          {btnValue}
         </Button>
       </Frm.Group>
     </Frm>
